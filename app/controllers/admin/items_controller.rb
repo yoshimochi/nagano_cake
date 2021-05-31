fd0b5d876ef61f33b85eeb944fb2448d8c0bb452
yoshimochi <olive.oil6319@gmail.com>
2021-05-31 14:45:38 +0000

    管理者_商品登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :asc).page(params[:page]).per(10)
   end
 
   def new
@@ -14,16 +14,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[])
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to '/admin/items'
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active)
+    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active, :image)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :genre
   attachment :image
 
-  belongs_to :genre
-
   validates :is_active, inclusion: { in: [true, false]}
+
+  def add_tax_price
+    (self.price * 1.08).round
+  end
 
 end
