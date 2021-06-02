7bead5631c391d3c350e0ef0ee5a13fd9b71dd0c
yoshimochi <olive.oil6319@gmail.com>
2021-06-02 14:57:44 +0000

    マイページ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+
   devise_for :customers, module: "customers"
+
+namespace :public do
+  get 'customers/mypage' => 'customers#show'
+  get 'customers/edit' => 'customers#edit'
+  patch 'customers' => 'customers#update'
+end
   # , controllers: {
   #   sessions:       'customers/sessions',
   #   passwords:      'customers/passwords',
   #   registrations:  'customers/registrations',
   # }
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
+  # namespace :admin do
+  #   get 'genres/index'
+  #   get 'genres/edit'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admins, path: 'admin',  module: "admins"
   # , controllers: {
@@ -22,6 +29,6 @@ Rails.application.routes.draw do
     get '/' => 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
-
   end
+
 end
