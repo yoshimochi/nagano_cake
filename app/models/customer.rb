2f22418598d0daadf00a6df5815330196a66c37a
yoshimochi <olive.oil6319@gmail.com>
2021-06-03 13:43:54 +0000

    管理者_顧客編集画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :is_active, inclusion: { in: [true, false]}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,28 +7,14 @@ namespace :public do
   get 'customers/edit' => 'customers#edit'
   patch 'customers' => 'customers#update'
 end
-  # , controllers: {
-  #   sessions:       'customers/sessions',
-  #   passwords:      'customers/passwords',
-  #   registrations:  'customers/registrations',
-  # }
-  # namespace :admin do
-  #   get 'genres/index'
-  #   get 'genres/edit'
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   devise_for :admins, path: 'admin',  module: "admins"
-  # , controllers: {
-  #   sessions:       'admin/sessions'
-  #   # passwords:      'admins/passwords',
-  #   # registrations:  'admins/registrations',
-  # }
-  # root 'homes#top'
 
   namespace :admin do
     get '/' => 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
 end
