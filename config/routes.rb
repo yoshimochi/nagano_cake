cdb8b0d16362d2999f8801401b50ac96a1cdf3fc
yoshimochi <olive.oil6319@gmail.com>
2021-06-03 12:22:18 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'genres/index'
     get 'genres/edit'
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admins, path: 'admin',  module: "admins"
